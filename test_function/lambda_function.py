9f68a38e3e9d5b13bde29df06ed2011ae7e87caa
imshubhampatel <shubhampatel2024@gmail.com>
2025-03-02 17:24:53 +0530

    Minor changes

--- test_function/lambda_function.py
+++ test_function/lambda_function.py
@@ -2,7 +2,8 @@ import json
 
 def lambda_handler(event, context):
     print('test')
+    print('test2')
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda 3!')
+        'body': json.dumps('Hello from Lambda 4!')
     }
