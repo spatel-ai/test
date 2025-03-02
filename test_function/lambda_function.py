4de419d01f86d097086713a063c36073d62c243f
imshubhampatel <shubhampatel2024@gmail.com>
2025-03-02 17:34:52 +0530

    Feat : the changes

--- test_function/lambda_function.py
+++ test_function/lambda_function.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     print('test2')
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda 444!')
+        'body': json.dumps('Hello from Lambda 9403!')
     }
