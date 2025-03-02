ddea9af24759a07c8f26b385a01638b93864188e
imshubhampatel <shubhampatel2024@gmail.com>
2025-03-02 17:28:55 +0530

    Added : changes

---
+++ test_function_2/lambda_function.py
@@ -0,0 +1,9 @@
+import json
+
+def lambda_handler(event, context):
+    print('test')
+    print('test2')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda 55!')
+    }
