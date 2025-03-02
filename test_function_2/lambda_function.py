4125f583cde5c72cfa7b959b3efad1c71a8b53c7
imshubhampatel <shubhampatel2024@gmail.com>
2025-03-02 17:33:53 +0530

    Feat : added changes

--- test_function_2/lambda_function.py
+++ test_function_2/lambda_function.py
@@ -2,8 +2,7 @@ import json
 
 def lambda_handler(event, context):
     print('test')
-    print('test2')
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda 555!')
+        'body': json.dumps('Hello from Lambda 999!')
     }
